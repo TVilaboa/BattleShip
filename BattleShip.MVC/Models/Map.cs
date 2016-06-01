7ec3d1781a1f0d1b4623ed3fced3900e69ee5743
Franco Testori <franco.testori@ing.austral.edu.ar>
2016-06-01 19:36:32 -0300

    Added Battleship Model Classes

---
+++ BattleShip.MVC/Models/Map.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BattleShip.MVC.Models
+{
+    public class Map
+    {
+        public List<List<Position>> Positions { get; set; }
+
+        public List<Ship> Ships { get; set; }
+
+        public List<Hit> Hits { get; set; }
+    }
+}
---
+++ BattleShip.MVC/Models/Position.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BattleShip.MVC.Models
+{
+    public class Position
+    {
+        public int XPosition { get; set; }
+        public int YPosition { get; set; }
+    }
+}
