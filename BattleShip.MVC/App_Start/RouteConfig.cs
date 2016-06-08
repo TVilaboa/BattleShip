696a3ecb557c7b65e7f3925c2ca59594fd5cd2b9
TVilaboa <tvilaboa@hotmail.com>
2016-06-07 22:42:42 -0300

    Fixes

--- BattleShip.MVC/App_Start/RouteConfig.cs
+++ BattleShip.MVC/App_Start/RouteConfig.cs
@@ -7,6 +7,7 @@ namespace BattleShip.MVC
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            routes.IgnoreRoute("elmah.axd");
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
